7eaa13bfbab34fb9e97e5d2f7f87b11a70673cfb
Artsiom Padhaiski <artempodgaisky@gmail.com>
2025-05-11 20:11:46 +0300

    fix fucking settings.py again

--- l5/settings.py
+++ l5/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
 
     'news',
     'vacancies',
-    'faq'
+    'faq',
+    'service_center'
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'l5.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "html"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
