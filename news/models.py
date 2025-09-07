17f37c13fe6631546c776c6aaa98cbd9221e3af2
Artsiom Padhaiski <artempodgaisky@gmail.com>
2025-09-07 17:41:23 +0300

    change news for the lw 1

--- news/models.py
+++ news/models.py
@@ -5,6 +5,7 @@ from django.utils.timezone import now
 
 class News(models.Model):
     head = models.CharField(max_length=255)
+    brief_content = models.TextField()
     content = models.TextField()
     date = models.DateField(default=now)
     image = models.ImageField(upload_to='news/')
