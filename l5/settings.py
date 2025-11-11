1f589565f56416ae0ab5b0b6a0f740d237be18ee
Artsiom Padhaiski <artempodgaisky@gmail.com>
2025-11-11 13:00:38 +0300

    Add constance

--- l5/settings.py
+++ l5/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'constance',
+    'constance.backends.database',
+
     'news',
     'vacancies',
     'faq',
@@ -166,3 +169,12 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# Django constance config
+CONSTANCE_BACKEND = 'constance.backends.database.DatabaseBackend'
+
+CONSTANCE_CONFIG = {
+        'SLIDER_TIMEOUT': (2000, "Timeout before slider picture changes"),
+    }
