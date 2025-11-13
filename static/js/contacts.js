893c9284b7614c15d288456f42004b3ba4e1bde6
Artsiom Padhaiski <artempodgaisky@gmail.com>
2025-11-13 19:08:41 +0300

    Format

--- static/js/contacts.js
+++ static/js/contacts.js
@@ -1,53 +1,62 @@
-const API_ENDPOINT = '/service/api/contacts';
+const API_ENDPOINT = "/service/api/contacts";
 const TABLE_ELEMENT_ID = "contacts";
 
+//global state variables
+let gContactsData = null;
+let gContactsSliceBuilder = null;
+let gActiveFilterProperty = null;
 
-class DataSliceBuilder{
-
+class DataSliceBuilder {
+  //filtered and sorted data
   data = null;
+
+  //paginated data
   slice = null;
 
-  constructor(data){
+  constructor(data) {
     this.data = data.slice();
   }
 
-  sortBy(propertyName, desc){
-    this.data.sort((a,b)=> {
-      if(a[propertyName] < b[propertyName]){
+  sortBy(propertyName, desc) {
+    this.data.sort((a, b) => {
+      if (a[propertyName] < b[propertyName]) {
         return desc ? +1 : -1;
-      } else if (a[propertyName] > b[propertyName]){
+      } else if (a[propertyName] > b[propertyName]) {
         return desc ? -1 : +1;
       }
       return 0;
-    })
+    });
     return this;
   }
 
-  filterBy(propertyName, filter){
-    this.data = this.data.filter((item) => item[propertyName].toString().includes(filter));
+  filterBy(propertyName, filter) {
+    this.data = this.data.filter((item) =>
+      item[propertyName].toString().includes(filter),
+    );
     return this;
   }
 
-  goToPage(page, itemsPerPage){
-    this.slice = this.data.slice(itemsPerPage * page, itemsPerPage * (page + 1));
+  goToPage(page, itemsPerPage) {
+    this.slice = this.data.slice(
+      itemsPerPage * page,
+      itemsPerPage * (page + 1),
+    );
     return this;
   }
 
-  length(){
+  length() {
     return this.build().length();
   }
 
-  build(){
-    if(this.slice === null){
+  build() {
+    if (this.slice === null) {
       return this.data;
     }
     return this.slice;
   }
-
 }
 
 class Employee {
-
   phone_number = null;
   image = null;
   specification = null;
@@ -57,8 +66,7 @@ class Employee {
   selected = false;
   index = null;
 
-
-  static fromJson(obj){
+  static fromJson(obj) {
     let emp = new Employee();
     emp.phone_number = obj.phone_number;
     emp.image = obj.image;
@@ -70,18 +78,20 @@ class Employee {
     return emp;
   }
 
-  intoTr(){
+  intoTr() {
     let tr = document.createElement("tr");
 
-    function createTd(content){
+    function createTd(content) {
       let td = document.createElement("td");
       td.innerHTML = content;
-      return td
+      return td;
     }
 
-    tr.appendChild(createTd(`<input type="checkbox"
+    tr.appendChild(
+      createTd(`<input type="checkbox"
                               data-idx=${this.index}
-                              ${this.selected ? "checked" : ""}>`));
+                              ${this.selected ? "checked" : ""}>`),
+    );
     tr.appendChild(createTd(`<img src="${this.image}">`));
     tr.appendChild(createTd(this.first_name));
     tr.appendChild(createTd(this.last_name));
@@ -91,11 +101,9 @@ class Employee {
 
     return tr;
   }
-
 }
 
 class ContactsTable {
-
   element = null;
   shownData = null;
 
@@ -106,78 +114,72 @@ class ContactsTable {
     this.show_data();
   }
 
-  clear_data(){
-    this.element.querySelectorAll("tbody tr").forEach((tr)=> {
+  clear_data() {
+    this.element.querySelectorAll("tbody tr").forEach((tr) => {
       tr.remove();
-    })
+    });
   }
 
-  show_data(){
-    for (let i = 0; i < this.shownData.length; i++){
-      this.element.querySelector("tbody").appendChild(this.shownData[i].intoTr());
+  show_data() {
+    for (let i = 0; i < this.shownData.length; i++) {
+      this.element
+        .querySelector("tbody")
+        .appendChild(this.shownData[i].intoTr());
     }
   }
 
-  set_data(newData){
+  set_data(newData) {
     this.shownData = newData;
     this.clear_data();
     this.show_data();
   }
-
 }
 
-async function fetchContacts(id){
+async function fetchContacts(id) {
   return fetch(API_ENDPOINT)
-    .then(async response => {
+    .then(async (response) => {
       if (!response.ok) {
-        throw new Error(`HTTP error. Status : ${response.status} - ${response.statusText}`);
+        throw new Error(
+          `HTTP error. Status : ${response.status} - ${response.statusText}`,
+        );
       }
-      let employees_data =  (await response.json()).employees.map((emp, idx) => {
+      let employees_data = (await response.json()).employees.map((emp, idx) => {
         let employee = Employee.fromJson(emp);
         employee.index = idx;
         return employee;
       });
       return employees_data;
     })
-    .catch(e => {
+    .catch((e) => {
       console.log(e);
       return null;
     });
 }
 
-
-
-let gContactsData = null;
-let gContactsSliceBuilder = null;
-let gActiveFilterProperty = null;
-
-
-function resetDataSliceBuilder(){
+function resetDataSliceBuilder() {
   return new DataSliceBuilder(gContactsData);
 }
 
-function applyFilter(){
-  if(gActiveFilterProperty === null){
+function applyFilter() {
+  if (gActiveFilterProperty === null) {
     alert("Select property to apply filter on");
     return;
   }
   let filterInput = document.getElementById("filter-input");
   gContactsSliceBuilder = resetDataSliceBuilder();
-  let filteredData =
-    gContactsSliceBuilder
+  let filteredData = gContactsSliceBuilder
     .filterBy(gActiveFilterProperty, filterInput.value)
-      .build();
+    .build();
   new ContactsTable(TABLE_ELEMENT_ID, filteredData);
 }
 
-
-function addSortOnClick(element){
-  if(element.dataset.prop == null){
+function addSortOnClick(element) {
+  if (element.dataset.prop == null) {
     return;
   }
   let clickCounter = 0;
 
-  function buildIcon(down){
+  function buildIcon(down) {
     let i = document.createElement("i");
     i.classList.add("fa-solid");
     i.classList.add(down ? "fa-sort-down" : "fa-sort-up");
@@ -185,17 +187,16 @@ function addSortOnClick(element){
     return i;
   }
 
-  element.addEventListener('click', ()=>{
-
+  element.addEventListener("click", () => {
     let sortIcon = document.getElementById("sort-icon");
     let desc = false;
-    if(sortIcon != null){
+    if (sortIcon != null) {
       sortIcon.remove();
     }
-    if(clickCounter == 0){
+    if (clickCounter == 0) {
       element.appendChild(buildIcon(false));
       clickCounter = 1;
-    } else if (clickCounter == 1){
+    } else if (clickCounter == 1) {
       element.appendChild(buildIcon(true));
       clickCounter = 2;
       desc = true;
@@ -205,21 +206,18 @@ function addSortOnClick(element){
       return;
     }
 
-    let sortedData =
-        gContactsSliceBuilder
-        .sortBy(element.dataset.prop, desc)
-        .build();
+    let sortedData = gContactsSliceBuilder
+      .sortBy(element.dataset.prop, desc)
+      .build();
     new ContactsTable(TABLE_ELEMENT_ID, sortedData);
-  })
-
+  });
 }
 
-
-function addFilters(element){
-  if(element.dataset.prop == null){
+function addFilters(element) {
+  if (element.dataset.prop == null) {
     return;
   }
-  function buildButton(){
+  function buildButton() {
     let i = document.createElement("i");
     i.classList.add("fa-solid");
     i.classList.add("fa-filter");
@@ -231,27 +229,26 @@ function addFilters(element){
   let btn = buildButton();
   element.appendChild(btn);
 
-  btn.addEventListener('click', (ev)=> {
+  btn.addEventListener("click", (ev) => {
     ev.stopPropagation();
-    if(btn.id == "filter-btn-active"){
+    if (btn.id == "filter-btn-active") {
       new ContactsTable(TABLE_ELEMENT_ID, gContactsData);
       btn.id = "";
       gActiveFilterProperty = null;
       return;
     }
     let activeFiler = document.getElementById("filter-btn-active");
-    if(activeFiler != null){
+    if (activeFiler != null) {
       activeFiler.id = "";
     }
     btn.id = "filter-btn-active";
 
     gActiveFilterProperty = element.dataset.prop;
     applyFilter();
-  })
+  });
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
-
+document.addEventListener("DOMContentLoaded", async () => {
   gContactsData = await fetchContacts();
   if (gContactsData === null) {
     document.getElementById("contacts").remove();
@@ -263,15 +260,14 @@ document.addEventListener('DOMContentLoaded', async () => {
   new ContactsTable(TABLE_ELEMENT_ID, gContactsData);
 
   // set up buttons for sorting and filter at each column
-  document.getElementById(TABLE_ELEMENT_ID)
+  document
+    .getElementById(TABLE_ELEMENT_ID)
     .querySelectorAll("thead td")
     .forEach((td) => {
       addSortOnClick(td);
       addFilters(td);
     });
 
-
   // set up handler for `find` button
-  document.getElementById("find-btn").addEventListener('click', applyFilter);
-
-})
+  document.getElementById("find-btn").addEventListener("click", applyFilter);
+});
