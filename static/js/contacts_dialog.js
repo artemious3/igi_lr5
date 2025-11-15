5a45a6b9df654300b1949ed5bdc7b63bc50c3a44
Artsiom Padhaiski <artempodgaisky@gmail.com>
2025-11-15 18:07:18 +0300

    Start adding dialog

---
+++ static/js/contacts_dialog.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('new-btn').addEventListener('click', () =>{
+    document.getElementById('dialog').showModal();
+  })
+});
